82bd8536fca78c7ee7b12c3d127521be70b49922
Tamzid Chowdhury <tamzidchowdhury@Tamzid-Chowdhury.local>
2021-04-23 17:18:25 -0400

    refactered

--- typedefs/region-def.js
+++ typedefs/region-def.js
@@ -1,29 +1,6 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql `
-	type Region {
-		_id: String!
-		name: String
-		capital: String
-        leader: String
-        flag: String
-        landmarks: [String!]
-        parentRegion: String
-    }
-    
-	extend type Query {
-        getAllRootRegions: [Region!]
-        getParentRegion: Region
-        getAllChildRegions: [Region!]
-        getAllSiblingRegions: [Region!]
-        getAllAncestorRegions: [Region!]
-        getRegionById(_id: String!): Region 
-    }
-    
-	extend type Mutation {
-
-    }
-    
 `;
 
 module.exports = { typeDefs: typeDefs }
--- typedefs/user-def.js
+++ typedefs/user-def.js
@@ -1,23 +1,7 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql `
-	type User {
-		_id: String
-		name: String
-		email: String
-		password: String
-    }
-    
-	extend type Query {
-		getCurrentUser: User
-    }
-    
-	extend type Mutation {
-		login(email: String!, password: String!): User
-        register(name: String!, email: String!, password: String!): User
-        updateAccount(name: String!, email: String!, password: String!): User
-		logout: Boolean!
-	}
+
 `;
 
 module.exports = { typeDefs: typeDefs }
